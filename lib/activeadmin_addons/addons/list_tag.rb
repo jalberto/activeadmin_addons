fb3b03e4cb5f2df046db4dab155667a4d4708df8
JA <ja@josealberto.org>
2016-09-14 17:45:03 +0100

    1st version

---
+++ lib/activeadmin_addons/addons/list_tag.rb
@@ -0,0 +1,31 @@
+class ListBuilder < ActiveAdminAddons::CustomBuilder
+  def render
+    return if data.nil?
+    raise "list must be Array" if !data.is_a?(Array)
+    render_list(data)
+  end
+
+  def render_list(_data)
+    _data.each do |value|
+      context.content_tag(:span) do
+        # context.concat(context.content_tag(:li, value))
+        value
+      end
+    end
+  end
+end
+
+module ::ActiveAdmin
+  module Views
+    class TableFor
+      def listtag_column(*args, &block)
+        column(*args) { |model| ::ListBuilder.new(self, model, *args, &block).render }
+      end
+    end
+    class AttributesTable
+      def listtag_row(*args, &block)
+        row(*args) { |model| ::ListBuilder.new(self, model, *args, &block).render }
+      end
+    end
+  end
+end
