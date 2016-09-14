6d4960af148e3b4d9da76174c802dee091fdc19f
JA <ja@josealberto.org>
2016-09-14 17:48:05 +0100

    Add require

--- lib/activeadmin_addons/engine.rb
+++ lib/activeadmin_addons/engine.rb
@@ -13,6 +13,7 @@ module ActiveAdminAddons
         require_relative './addons/state_values'
         require_relative './addons/number'
         require_relative './addons/list'
+        require_relative './addons/list_tag'
         require_relative './support/enumerize_formtastic_support'
         require_relative './support/set_datepicker'
         app.config.assets.precompile += %w(select.scss fileicons/*.png)
